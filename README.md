e202d5328fc0d64c695198c3cf4297f4bddb4f68
farhan-nahid <nahidahmed9933@gmail.com>
2021-07-19 18:44:50 +0600

    done

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 ## [English Premier League](https://englishpremiumleague.netlify.app/)
 
-
 ## Main Part
-* home page banner
-* 3 cards in a row in desktop view
-* daynamic details banner for every team
-* conditional formeting for male & female
-* using thesportsdb api 
-* using react-route
-* using fontawesome icon
+
+- home page banner
+- 3 cards in a row in desktop view
+- Dynamic details banner for every team
+- conditional formeting for male & female
+- using thesportsdb api
+- using react-route
+- using font awesome icon
 
 ## Bonus Part
-* website is responsive for any device(desktop, laptop, tab, mobile)
-* change design & website Name
-* add hover & shadow  effect in card
-* linking the social icons.
-*  add navbar (click on the name you can change the path)
+
+- website is responsive for any device(desktop, laptop, tab, mobile)
+- change design & website Name
+- add hover & shadow effect in card
+- linking the social icons.
+- add navbar (click on the name you can change the path)
--- src/Component/TeamDetails/TeamDetails.js
+++ src/Component/TeamDetails/TeamDetails.js
@@ -1,76 +1,123 @@
-import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faFlag, faFutbol, faMapMarkerAlt, faVenusMars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
-import { Card, Image } from 'react-bootstrap';
-import { useParams } from 'react-router';
-import femaleImage from '../../img/female.png';
-import maleImage from '../../img/male.png';
-import './TeamDetails.css';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faFlag,
+  faFutbol,
+  faMapMarkerAlt,
+  faVenusMars,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Card, Image } from "react-bootstrap";
+import { useParams } from "react-router";
+import femaleImage from "../../img/female.png";
+import maleImage from "../../img/male.png";
+import "./TeamDetails.css";
 
 const TeamDetails = () => {
-   const { idTeam } = useParams();
+  const { idTeam } = useParams();
 
-   const [team , setTeam ] = useState([]);
-   
-   const{strStadiumThumb  , strTeamBadge, strDescriptionEN , strStadiumDescription , strTeam , intFormedYear , strCountry , strSport , strGender , strTwitter , strWebsite , strYoutube} = team;
+  const [team, setTeam] = useState([]);
 
-   useEffect( () =>{
-       const url=`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`
-       fetch(url)
-        .then (res => res.json())
-        .then(data => setTeam(data.teams[0]))
-   }, [idTeam])
+  const {
+    strStadiumThumb,
+    strTeamBadge,
+    strDescriptionEN,
+    strStadiumDescription,
+    strTeam,
+    intFormedYear,
+    strCountry,
+    strSport,
+    strGender,
+    strTwitter,
+    strWebsite,
+    strYoutube,
+  } = team;
 
-   
-    return (
-        
-        <>
-            <Card>
-                <Card.Img className="stadium-image" src={strStadiumThumb} alt="..." />
-                <div>
-                    <Card.ImgOverlay>
-                        <Image className="team-logo margin-auto" src={strTeamBadge } alt="..." />
-                    </Card.ImgOverlay>
-                </div>
-            </Card>
-            <div className="player-details">
-                 <div className="container ">
-                    <section className="about-team mt-5" >
-                        <div className="row d-flex">
-                            <div className="col-md-6  p-5">
-                                <h2>{strTeam}</h2>
-                                <h5><FontAwesomeIcon icon={faMapMarkerAlt} /> Founded : {intFormedYear}</h5>
-                                <h5><FontAwesomeIcon icon={faFlag} /> Country : {strCountry}</h5>
-                                <h5><FontAwesomeIcon icon={faFutbol} /> Sport Type : {strSport}</h5>
-                                <h5><FontAwesomeIcon icon={faVenusMars} /> Gender : {strGender}</h5>
-                            </div>
-                            <div className="col-md-6  conditional-image">
-                                {
-                                    strGender ==="Male" ? <img src={maleImage} alt="..." /> : <img src={femaleImage} alt="..." />
-                                }
-                            </div>
-                        </div>
-                    </section>
-                <section className="mt-5">
-                        <p>{strDescriptionEN}</p>
-                        <p className="mt-5">{strStadiumDescription}</p>
-                </section>
-                <section className="social-link text-center mt-5 mb-5">
-                    <a target="-blank" href={`https://${strTwitter}`} className="twitter text-info mr-3">
-                            <FontAwesomeIcon icon={faTwitter}  />
-                    </a>
-                    <a target="-blank" href={`https://${strWebsite}`} className="facebook text-primary ">
-                        <FontAwesomeIcon icon={faFacebook} />
-                    </a>
-                    <a target="-blank" href={`https://${strYoutube}`} className="youTube text-danger ml-3">
-                            <FontAwesomeIcon icon={faYoutube} />
-                     </a>
-                </section>
-                </div>
+  useEffect(() => {
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTeam(data.teams[0]));
+  }, [idTeam]);
+
+  return (
+    <>
+      <Card>
+        <Card.Img className="stadium-image" src={strStadiumThumb} alt="..." />
+        <div>
+          <Card.ImgOverlay>
+            <Image
+              className="team-logo margin-auto"
+              src={strTeamBadge}
+              alt="..."
+            />
+          </Card.ImgOverlay>
+        </div>
+      </Card>
+      <div className="player-details">
+        <div className="container ">
+          <section className="about-team mt-5">
+            <div className="row d-flex">
+              <div className="col-md-6  p-5">
+                <h2>{strTeam}</h2>
+                <h5>
+                  <FontAwesomeIcon icon={faMapMarkerAlt} /> Founded :{" "}
+                  {intFormedYear}
+                </h5>
+                <h5>
+                  <FontAwesomeIcon icon={faFlag} /> Country : {strCountry}
+                </h5>
+                <h5>
+                  <FontAwesomeIcon icon={faFutbol} /> Sport Type : {strSport}
+                </h5>
+                <h5>
+                  <FontAwesomeIcon icon={faVenusMars} /> Gender : {strGender}
+                </h5>
+              </div>
+              <div className="col-md-6  conditional-image">
+                {strGender === "Male" ? (
+                  <img src={maleImage} alt="..." />
+                ) : (
+                  <img src={femaleImage} alt="..." />
+                )}
+              </div>
             </div>
-        </>
-    );
+          </section>
+          <section className="mt-5">
+            <p>{strDescriptionEN}</p>
+            <p className="mt-5">{strStadiumDescription}</p>
+          </section>
+          <section className="social-link text-center mt-5 mb-5">
+            <a
+              target="-blank"
+              href={`https://${strTwitter}`}
+              className="twitter text-info mr-3"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a
+              target="-blank"
+              href={`https://${strWebsite}`}
+              className="facebook text-primary "
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              target="-blank"
+              href={`https://${strYoutube}`}
+              className="youTube text-danger ml-3"
+            >
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TeamDetails;
